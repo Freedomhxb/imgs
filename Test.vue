8a2129fb790e5f9f0adf7e36b05b27ec12c2e93a
韩旭斌 <jujj8421@foxmail.com>
2021-11-18 17:21:53 +0800

    Create Test.vue

---
+++ Test.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <scroller style="flex: 1;">
+            <div style="flex-direction: row;border-style: solid;border-color: rosybrown;border-width: 1px;margin-top: 20px">
+                <image style="width: 100px;height: 100px;background-color: #afddff"></image>
+                <div style="flex: 1">
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+                    <text>{{ content }}</text>
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+                    <div
+                        style="margin-top:10px;height: 50px;background-color: #717171;border-width: 2px;border-color: #41B883;border-style: solid"></div>
+
+                </div>
+            </div>
+
+        </scroller>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Test",
+    data() {
+        return {
+            content: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
