527d72a9d526d2a8e50feeecc31f69ad7e3a3f68
kk <jujj8421@foxmail.com>
2021-11-19 15:01:52 +0800

    Update Test.vue

--- Test.vue
+++ Test.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <scroller style="flex: 1;">
+     
             <div style="flex-direction: row;border-style: solid;border-color: rosybrown;border-width: 1px;margin-top: 20px">
                 <image style="width: 100px;height: 100px;background-color: #afddff"></image>
                 <div style="flex: 1">
@@ -23,7 +23,6 @@
                 </div>
             </div>
 
-        </scroller>
     </div>
 </template>
 
